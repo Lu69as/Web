4c2e7c211e3f51ace5caf11fc85444f73f809c04
Lukas Okkenhaug <lukasokken@gmail.com>
2022-12-07 12:56:43 +0100

    Version 2.0

---
+++ Html/Main.js
@@ -0,0 +1,16 @@
+/* Alle de forskjellige knappene */
+function Btn() {
+    document.getElementById("Drop").classList.toggle("Drop_show");
+}
+function Btn2() {
+    document.getElementById("Drop2").classList.toggle("Drop_show2");
+}
+function Btn3() {
+    document.getElementById("Drop3").classList.toggle("Drop_show3");
+}
+function Btn4() {
+    document.getElementById("Drop4").classList.toggle("Drop_show4");
+}
+function Btn5() {
+    document.getElementById("Drop5").classList.toggle("Drop_show5");
+}
