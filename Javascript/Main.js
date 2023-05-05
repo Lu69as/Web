214096fb2f858db6186564f3122e26e0fca76044
lu69as <lukasokken@gmail.com>
2023-05-05 12:19:03 +0200

    Small, but important update

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,22 +1,34 @@
 // creates a variable for the url of the site
 const index = "index.html#";
 const url = window.location.href;
-if (url === "https://lu69as.github.io/Web/") {
+
+// If the url is github it makes the link have index.html at the end
+if (url === "https://lu69as.github.io/Web/") 
+{
     window.location.href = url + index;
 }
-else if (url === "http://127.0.0.1:5500/") {
+// Does the same, but for the localhost site
+else if (url === "http://127.0.0.1:5500/") 
+{
     window.location.href = url + index;
 }
 
+// Declares the height and with variable
+// so the site knows the device screen size
+let height = innerHeight;
+let width = innerWidth;
+
 // Function for pasting the footer so I dont have to put in every site, 
 // It can automatically paste it into any site that has the id in a div.
-if (url.includes("Foto") === false) {
-    document.getElementById("footer").innerHTML =
-        `<div class="container p-4 pb-0">
+if (url.includes("Foto") === false) 
+{
+    document.querySelector("#footer").innerHTML =
+    `
+        <div class="container p-4 pb-0">
         <section class="mb-4">
     
         <!-- Help -->
-        <button class="btn text-white btn-floating m-1" onclick="alert(help)">
+        <button class="btn text-white btn-floating m-1" style="cursor: help;" onclick="alert(help)">
         <i class="fa-solid fa-circle-question"></i>
         </button>
     
@@ -51,11 +63,12 @@ if (url.includes("Foto") === false) {
         <i class="fab fa-reddit"></i></a>
     
         <!-- Instagram -->
-        <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac;"
+        <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac; cursor: help;"
         href="https://www.instagram.com/lu69as/" role="button" target="_blank">
         <i class="fab fa-instagram"></i></a>
     
-        <button class="btn text-white btn-floating m-1" onclick="alert(care)">
+        <button class="btn text-white btn-floating m-1" style="cursor: help;"
+        onclick="alert('I dont really care, take it with HR')">
         <i class="fa-solid fa-bug"></i>
         </button>
     
@@ -65,7 +78,8 @@ if (url.includes("Foto") === false) {
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2023 Copyright: Lukas Okkenhaug
-    </div>`
+    </div>
+`
 }
 
 // Creates a variable for the "help" message in the footer
@@ -74,133 +88,153 @@ If you see a lack of information somewhere
             Just click a lot!`
 
 // Function that does the same as the footer but for the navbar
-if (url.includes("index")) {
-    // If the url has index it pastes this navbar
-    document.getElementById("navbar").innerHTML =
+// If the url has index in it and the device width is higher than 1000px it pastes this navbar
+if (url.includes("index") && width > 1000) 
+{
+    document.querySelector("#navbar").innerHTML =
         `<div class="container-fluid">
             <ul class="navbar-nav">
 
                 <!-- Hjemmesiden -->
                 <li class="nav-item">
-                <a class="nav-link"aria-current="page" href="#"><i class="fas fa-home-alt"></i> Home </a>
+                    <a class="nav-link active hom" onclick="activehome()" href="#"><i class="fas fa-home-alt"></i> Home </a>
                 </li>
 
                 <!-- Arbeidserfaring side -->
                 <li class="nav-item">
-                <a class="nav-link" id="translate1" href="#work_xp"></a>
+                    <a class="nav-link" id="translate1" href="#work_xp" onclick="activework()"></a>
                 </li>
 
                 <!-- Utdanning side -->
                 <li class="nav-item">
-                <a class="nav-link" id="translate2" href="#education"></a>
+                    <a class="nav-link" id="translate2" href="#education" onclick="activeedu()"></a>
                 </li>
 
                 <!-- Hobbier side -->
                 <li class="nav-item">
-                <a class="nav-link" id="translate3" href="#hobbies"></a>
+                    <a class="nav-link" id="translate3" href="#hobbies" onclick="activehobby()"></a>
                 </li>
 
                 <!-- Other sites -->
                 <li class="nav-item">
-                <a class="nav-link" id="translate70" href="#misc"></a>
+                    <a class="nav-link" id="translate4" href="#misc" onclick="activemisc()"></a>
                 </li>
 
                 <li class="nav-item">
-                <button onclick="changelang();" id="BtnBorder"> <i class="fa-solid fa-language"></i></button>
+                    <button onclick="changelang();" id="BtnBorder"> <i class="fa-solid fa-language"></i></button>
                 </li>
             </ul>
         </div>
     </div>`
-
-    if (innerWidth < 1000) {
-        document.getElementById("navbar").innerHTML =
-            `
-            <div class="container-fluid">
-            <ul class="navbar-nav">
-                <!-- Hjemmesiden -->
-                <li class="nav-item">
-                <a class="nav-link"aria-current="page" href="#"><i class="fas fa-home-alt"></i> Home 
-                <button onclick="changelang();" id="BtnBorder"> <i class="fa-solid fa-language"></i></button></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    `
-    }
 }
 else if (url.includes("Foto"))
 // If you are on the Photoes site, it gives a less distracting navbar that just has the "home" button
 {
-    document.getElementById("navbar").innerHTML =
+    document.querySelector("#navbar").innerHTML =
         `<div class="container-fluid">
         <ul class="navbar-nav">
             <!-- Hjemmesiden -->
             <li class="nav-item">
-            <a class="nav-link" id="site1N" aria-current="page" href="../index.html"><i class="fas fa-home-alt"></i> Home </a>
+            <a class="nav-link active" id="site1N" aria-current="page" href="../index.html"><i class="fas fa-home-alt"></i> Home </a>
             </li>
         </ul>
     </div>`
 }
+else
+// If the device has a smaller width than 1000, the navbar will be minimized.
+{
+    document.querySelector("#navbar").innerHTML =
+    `
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <!-- Hjemmesiden -->
+            <li class="nav-item">
+            <a class="nav-link active"aria-current="page" href="#"><i class="fas fa-home-alt"></i> Home </a>
+            </li>
+            <li class="nav-item">
+            <button onclick="changelang();" id="BtnBorder"> <i class="fa-solid fa-language"></i></button>
+            </li>
+        </ul>
+    </div>
+    </div>
+    `
+}
 
 // Changes the language of the site
 // It remembers what language you last were on and uses just that
-function changelang() {
-    if (localStorage.getItem('lang') === 'en') {
+function changelang() 
+{
+    // If the language is set 
+    if (localStorage.getItem('lang') === 'en') 
+    {
         localStorage.setItem('lang', 'no')
     }
-    else {
+    else 
+    {
         localStorage.setItem("lang", "en")
     }
-    location.reload();
+    translate();
 }
 // If the language isnt set, it is automatically english
-if (localStorage.getItem('lang') === null) {
+if (localStorage.getItem('lang') === null) 
+{
     localStorage.setItem('lang', 'en')
 }
 
 /* All the different drop buttons for the "hobbies" site */
-function Btn() {
-    document.getElementById("Drop").classList.toggle("Drop_show");
+function Btn() 
+{
+    document.querySelector("#Drop").classList.toggle("Drop_show");
 }
 
-function Btn2() {
-    document.getElementById("Drop2").classList.toggle("Drop_show2");
+function Btn2() 
+{
+    document.querySelector("#Drop2").classList.toggle("Drop_show2");
 }
 
-function Btn3() {
-    document.getElementById("Drop3").classList.toggle("Drop_show3");
+function Btn3() 
+{
+    document.querySelector("#Drop3").classList.toggle("Drop_show3");
 }
 
-function Btn4() {
-    document.getElementById("Drop4").classList.toggle("Drop_show4");
+function Btn4() 
+{
+    document.querySelector("#Drop4").classList.toggle("Drop_show4");
 }
 
-function Btn5() {
-    document.getElementById("Drop5").classList.toggle("Drop_show5");
+function Btn5() 
+{
+    document.querySelector("#Drop5").classList.toggle("Drop_show5");
 }
 
-function Btn6() {
-    document.getElementById("Drop6").classList.toggle("Drop_show6");
+function Btn6() 
+{
+    document.querySelector("#Drop6").classList.toggle("Drop_show6");
 }
 
-function Btn7() {
-    document.getElementById("Drop7").classList.toggle("Drop_show7");
+function Btn7() 
+{
+    document.querySelector("#Drop7").classList.toggle("Drop_show7");
 }
 
-function Btn8() {
-    document.getElementById("Drop8").classList.toggle("Drop_show8");
+function Btn8() 
+{
+    document.querySelector("#Drop8").classList.toggle("Drop_show8");
 }
 
-function Btn9() {
-    document.getElementById("Drop9").classList.toggle("Drop_show9");
+function Btn9() 
+{
+    document.querySelector("#Drop9").classList.toggle("Drop_show9");
 }
 
-function Btn10() {
-    document.getElementById("Drop10").classList.toggle("Drop_show10");
+function Btn10() 
+{
+    document.querySelector("#Drop10").classList.toggle("Drop_show10");
 }
 
 // Small little easter egg
-function welcome() {
+function welcome() 
+{
     alert("Hi, I'm Lukas");
 
     if (localStorage.getItem("name") === null) {
@@ -227,20 +261,79 @@ function welcome() {
         alert("You still live at " + localStorage.getItem("place") + ` Wich is in ` + localStorage.getItem("country") + `,
         and your name is still ` + localStorage.getItem("name") + `
         No there are still no Female F1 drivers
-        Your information is still at Meta Headquarters.`)
+        Your information is still at Meta Headquarters.`);
     }
 
 }
 
 // Button for showing and removing the text aside the pictures of my schools
-function toggleinvis(x) {
-    document.getElementById("invis" + x).classList.toggle("invis")
+function toggleinvis(x) 
+{
+    document.querySelector("#invis" + x).classList.toggle("invis");
 }
 
 // Bug report value
 let care = "I don't really care, take it with HR"
 
 // If you are in landscape mode in the "photoes" page this class will activate
-if (innerWidth > innerHeight && url.includes("oto")) {
-    document.getElementById("translate8").classList.toggle("LandscapeMargin")
+if (height > height && url.includes("oto")) 
+{
+    document.querySelector("#translate8").classList.toggle("LandscapeMargin");
+}
+
+// Button for opening the movie ratings table
+function togglemovie() 
+{
+    document.querySelector("#togglemovie").classList.toggle("invis");
+}
+// Button for opening the series ratings table
+function toggletv() 
+{
+    document.querySelector("#toggletv").classList.toggle("invis");
+}
+
+// Function for resetting the active blue color when you click a new link
+function refreshnav()
+{
+    document.querySelector(".hom").classList.remove("active");
+    document.querySelector("#translate1").classList.remove("active");
+    document.querySelector("#translate2").classList.remove("active");
+    document.querySelector("#translate3").classList.remove("active");
+    document.querySelector("#translate4").classList.remove("active");
+}
+
+// Makes the home button blue in the navbar
+function activehome()
+{
+    refreshnav();
+    document.querySelector(".hom").classList.add("active");
+}
+
+// Makes the work experience button blue in the navbar
+function activework()
+{
+    refreshnav();
+    document.querySelector("#translate1").classList.add("active");
+    window.location.href = url + "#work_xp";
+}
+
+// Makes the education buttun in the navbar blue
+function activeedu()
+{
+    refreshnav();
+    document.querySelector("#translate2").classList.add("active");
+}
+
+// Makes the hobbies button blue in the navbar
+function activehobby()
+{
+    refreshnav();
+    document.querySelector("#translate3").classList.add("active");
+}
+
+// Makes the games and other sites button blue in the navbar
+function activemisc()
+{
+    refreshnav();
+    document.querySelector("#translate4").classList.add("active");
 }
