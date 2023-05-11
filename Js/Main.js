fbd79ab3ad62e71104d3ac4e735dfd8655d527a4
lu69as <lukasokken@gmail.com>
2023-05-11 09:10:44 +0200

    Pog Update (I swear)

--- Js/Main.js
+++ Js/Main.js
@@ -18,12 +18,33 @@ else if (url === "http://127.0.0.1:5500/")
 let height = innerHeight;
 let width = innerWidth;
 
+// A small loading screen
+let loadtime = 600
+function loading() 
+{
+    document.querySelector("#loadingTxt").innerHTML = 'Loading';
+
+    setTimeout(() => { document.querySelector("#loadingTxt").innerHTML = 'Loading.' }, loadtime);
+    setTimeout(() => { document.querySelector("#loadingTxt").innerHTML = 'Loading..' }, loadtime * 2);
+    setTimeout(() => { document.querySelector("#loadingTxt").innerHTML = 'Loading...' }, loadtime * 3);
+
+    window.onload = () => 
+    {
+        loop = false
+        document.querySelector("#loading").classList.add("invis");
+    }
+}
+loading();
+for (let x = 1; x < 40; x++)
+{
+    setTimeout(() => { loading() }, loadtime * 4 * x);
+}
+
 // Function for pasting the footer so I dont have to put in every site, 
 // It can automatically paste it into any site that has the id in a div.
-if (url.includes("Foto") === false) 
-{
+if (url.includes("Foto") === false) {
     document.querySelector("#footer").innerHTML =
-    `
+        `
         <div class="container p-4 pb-0">
         <section class="mb-4">
     
@@ -84,8 +105,7 @@ if (url.includes("Foto") === false)
 
 // Function that does the same as the footer but for the navbar
 // If the url has index in it and the device width is higher than 1000px it pastes this navbar
-if (url.includes("index") && width > 1000) 
-{
+if (url.includes("index") && width > 1000) {
     document.querySelector("#navbar").innerHTML =
         `<div class="container-fluid">
             <ul class="navbar-nav">
@@ -139,7 +159,7 @@ else
 // If the device has a smaller width than 1000, the navbar will be minimized.
 {
     document.querySelector("#navbar").innerHTML =
-    `
+        `
     <div class="container-fluid">
         <ul class="navbar-nav">
             <!-- Hjemmesiden -->
@@ -151,131 +171,113 @@ else
             </li>
         </ul>
     </div>
-    </div>
-    `
-}
-
-// Changes the language of the site
+    </div>`//                      __
+                                  //\\
+}                                //  \\
+                                //    \\
+// Changes the language of the site    \\
 // It remembers what language you last were on and uses just that
-function changelang() 
-{
-    // If the language is set 
-    if (localStorage.getItem('lang') === 'en') 
-    {
-        localStorage.setItem('lang', 'no')
-    }
-    else 
-    {
-        localStorage.setItem("lang", "en")
-    }
-    translate();
-}
-
+function changelang() {      //          \\
+    // If the language is set/            \\
+    if (localStorage.getItem('lang') === 'en') {
+        localStorage.setItem('lang', 'no')//\\
+    }                   //                   \\
+    else {             //                     \\
+        localStorage.setItem("lang", "en")//   \\
+    }                //     __           __     \\
+    translate();    //     <__>         <__>     \\
+}                  //                             \\
+                  //                               \\
 // If the language isnt set, it is automatically english
-if (localStorage.getItem('lang') === null) 
-{
-    localStorage.setItem('lang', 'en')
-}
-
-/* All the different drop buttons for the "hobbies" site */
-function Btn() 
-{
+if (localStorage.getItem('lang') === null) {//       \\
+    localStorage.setItem('lang', 'en')//              \\
+}             //              ____________             \\         __
+             //               \__________/              \\       /*/
+/* All the different drop buttons for the "hobbies" site \\     /*/
+function Btn() {//                                        \\   /*/
     document.querySelector("#Drop").classList.toggle("Drop_show");
-}
-
-function Btn2() 
-{
+}        //                                                 \\*/
+        //                                                  /\\
+function Btn2() {//                                        /*/\\
     document.querySelector("#Drop2").classList.toggle("Drop_show2");
-}
-
-function Btn3() 
-{
-    document.querySelector("#Drop3").classList.toggle("Drop_show3");
-}
-
-function Btn4() 
-{
-    document.querySelector("#Drop4").classList.toggle("Drop_show4");
-}
-
-function Btn5() 
-{
+}    //                                                  /*/    \\
+    //                                                  /*/      \\
+function Btn3() {//                                    /*/        \\
+/*//*/document.querySelector("#Drop3").classList.toggle("Drop_show3");
+}//                                                  /*/            \\
+//                                                  /*/              \\
+function Btn4() {//                                /*/               //
+    document.querySelector("#Drop4").classList.toggle("Drop_show4");//
+}                                                                  //
+                                                                  //
+function Btn5() {//                                              //
     document.querySelector("#Drop5").classList.toggle("Drop_show5");
-}
-
-function Btn6() 
-{
+}                                                              //
+                                                              //
+function Btn6() {//                                          //
     document.querySelector("#Drop6").classList.toggle("Drop_show6");
-}
-
-function Btn7() 
-{
+}                                                          //
+                                                          //
+function Btn7() {//                                      //
     document.querySelector("#Drop7").classList.toggle("Drop_show7");
-}
-
-function Btn8() 
-{
+}                                                      //
+                                                      //
+function Btn8() {//                                  //
     document.querySelector("#Drop8").classList.toggle("Drop_show8");
-}
-
-function Btn9() 
-{
-    document.querySelector("#Drop9").classList.toggle("Drop_show9");
-}
-
-function Btn10() 
-{
+}                                                  //
+                                                  //
+function Btn9() {//                              //
+/*//*/document.querySelector("#Drop9").classList.toggle("Drop_show9");
+}//              \\                            //
+//                \\                          //
+function Btn10() {//                         //
     document.querySelector("#Drop10").classList.toggle("Drop_show10");
-}
-
-// ------------------------------------- //
-// Easter Egg Code Right here:           \\
-// ------------------------------------- //
-function welcome() 
-{
-    alert("Hi, I'm Lukas");
-
-    if (localStorage.getItem("country") == null || localStorage.getItem("country") === "") 
-    {
+}   //                                     //
+   //                                     //
+  // -----------------------------------_[x]
+ // Easter Egg Code Right here:        |  _|
+// ------------------------------------[x]
+function welcome() {                   //
+    alert("Hi, I'm Lukas");           //
+                                     //
+    if (localStorage.getItem("country") == null || localStorage.getItem("country") === "") {
         localStorage.setItem("name", prompt("Whats your name? Please say the full"));
-
-        if (localStorage.getItem("name") == null || localStorage.getItem("name") === "")
-        {
+                                  //
+        if (localStorage.getItem("name") == null || localStorage.getItem("name") === "") {
             alert("Fine then...");
-        }
-        else
-        {
+        }                      //
+        else {                //
             alert("Hello " + localStorage.getItem("name") + ", And welcome to my website!");
             alert("I am going to need a little more info though...");
-    
+                           //
             localStorage.setItem("place", prompt("Where do you live?"));
-    
+                         //
             localStorage.setItem("country", prompt("Which country is that in?"));
-    
+                       //
             alert("Ahh, you live at " + localStorage.getItem("place") + " , and your name is " + localStorage.getItem("name"));
-    
+                     //
             alert("I'm going to send this to Meta headquarters. Thank you for your service");
-    
-            if (localStorage.getItem("country").toLowerCase() === "norway" && localStorage.getItem("lang") === "en" 
-                || localStorage.getItem("country").toLowerCase() === "no" && localStorage.getItem("lang") === "en"); 
-            {
-                alert("Jeg valgte å bytte språk siden du er fra norge");
-                changelang();
-            }
-        }
-    }
-    else 
-    {
+                   //
+            if (localStorage.getItem("country").toLowerCase() === "norway" && localStorage.getItem("lang") === "en"
+             || localStorage.getItem("country").toLowerCase() === "no" && localStorage.getItem("lang") === "en");
+            {   //
+/*             //   */  alert("Jeg valgte å bytte språk siden du er fra norge"); changelang();
+              //
+            }//
+        }   //
+    }      //
+    else {//
         alert("You still live at " + localStorage.getItem("place") + ` Wich is in ` + localStorage.getItem("country") + `,
-        and your name is still ` + localStorage.getItem("name") + `
-        No there are still no Female F1 drivers
-        Your information is still at Meta Headquarters.`);
-    }
-
-}
-
-// Button for showing and removing the text aside the pictures of my schools
-function toggleinvis(x) 
+        and your name is still ` + localStorage.getItem("name") + 
+/*     //       */'Your information is still at Meta Headquarters.'+
+/*    //        */'No, Biden has still not died and neither has Trump');
+    }//
+    //
+}  //
+  //
+ // Button for showing and removing the text aside the pictures of my schools
+//
+ function toggleinvis(x) 
 {
     document.querySelector("#invis" + x).classList.toggle("invis");
 }
@@ -298,45 +300,44 @@ function toggletv()
 }
 
 // Function for resetting the active blue color when you click a new link
-function refreshnav()
+function refreshnav() 
 {
-    document.querySelector(".hom").classList.remove("active");
-    document.querySelector("#translate1").classList.remove("active");
-    document.querySelector("#translate2").classList.remove("active");
-    document.querySelector("#translate3").classList.remove("active");
-    document.querySelector("#translate4").classList.remove("active");
+    [".hom", "#translate1", "#translate2", "#translate3", "#translate4"].forEach((tag) => 
+    {
+        document.querySelector(tag).classList.remove("active")
+    })
 }
 
 // Makes the home button blue in the navbar
-function activehome()
+function activehome() 
 {
     refreshnav();
     document.querySelector(".hom").classList.add("active");
 }
 
 // Makes the work experience button blue in the navbar
-function activework()
+function activework() 
 {
     refreshnav();
     document.querySelector("#translate1").classList.add("active");
 }
 
 // Makes the education buttun blue in the navbar
-function activeedu()
+function activeedu() 
 {
     refreshnav();
     document.querySelector("#translate2").classList.add("active");
 }
 
 // Makes the hobbies button blue in the navbar
-function activehobby()
+function activehobby() 
 {
     refreshnav();
     document.querySelector("#translate3").classList.add("active");
 }
 
 // Makes the games and other sites button blue in the navbar
-function activemisc()
+function activemisc() 
 {
     refreshnav();
     document.querySelector("#translate4").classList.add("active");
